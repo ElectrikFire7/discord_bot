6ae93ecb9d1b76f2e23c2a345c3f80bc9b7c13c7
ElectrikFire7 <9kishlay@gmail.com>
2026-03-27 00:14:38 +0530

    new gifs added

--- simpleResponse/simpleResponse.py
+++ simpleResponse/simpleResponse.py
@@ -5,7 +5,7 @@ async def simpleResponse(message: Message):
     content = message.content.lower()
 
     if content == "hello":
-        await message.channel.send("Hello!")
+        await message.channel.send("Shalom!")
 
     elif content == "hello there":
         await message.channel.send("General Kenobi!")
@@ -17,3 +17,12 @@ async def simpleResponse(message: Message):
         await message.channel.send("FINALLY!! Someone is here")
         await message.channel.send("I was getting bored, and all I could do was")
         await message.channel.send(file = File("media/milk_burst.gif"))
+
+    elif "heil hitler" in content:
+        await message.channel.send(file = File("media/heil.gif"))
+
+    elif "shalom" in content:
+        await message.channel.send("😋🧱")
+
+    elif "allahu akbar" in content:
+        await message.channel.send(file = File("media/aha.gif"))
